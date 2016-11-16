e0146d7ea2880eefc2e538dc1830c71168c59a0e
ROMY\Romy96 <romybijkerk1996@gmail.com>
2016-11-16 08:42:39 +0100

    Edited the blade of register for a better and more beautiful design

---
+++ views/register.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+
+@section('content')
+
+<h1>Registreer</h1>
+
+    <div class="row">
+        <form role="form" method="post" action="register_action.php">
+            <div class="col-lg-6">
+                <div class="well well-sm"><strong><span class="glyphicon glyphicon-asterisk"></span>Required Field</strong></div>
+                <div class="form-group">
+                    <label for="InputEmail">Email</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="InputEmail" id="InputEmail" placeholder="Enter email" required>
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Password</label>
+                    <div class="input-group">
+                        <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Enter password">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
+                    </div>
+                </div>
+                <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info pull-right">
+            </div>
+        </form>
+    </div>
+
+@endsection
