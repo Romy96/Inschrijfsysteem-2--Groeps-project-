f8d9be6efb733900eef0ab5b6e07d447824a38ba
Lars Luijendijk <larsluijendijk@gmail.com>
2016-12-21 14:29:56 +0100

    De code zorgt voor het veranderen van de CSS als de gebruiker voor de activiteit is ingeschreven

--- views/event_activities.blade.php
+++ views/event_activities.blade.php
@@ -1,33 +1,39 @@
 @extends('layout')
-
+                
 @section('content')
-
+                
 	@foreach ( $events as $event )
 		<div class='banner' style='background-image:url({{$event['large_banner_url']}})'>	
 			<h1 class='title'>{{$event['title']}}</h1>
-		</div>
-	@endforeach
-
+		</div>  
+	@endforeach 
+                
 	@foreach ( $activities as $activity ) 
-	<div> 
+	<div>       
 		<form method="POST" action="select_action.php" onsubmit="return confirm('Weet je het zeker?');">
-		    <div class='img' style='background-image:url({{$activity['banner_url']}})'>
+            @if ($activity['ingeschreven'] === 0)
+            <div class='img' style='background-image:url({{$activity['banner_url']}}); border:solid; border-color:red;'> 
+            @elseif ($activity['ingeschreven'] > 1)
+            <div class='img' style='background-image:url({{$activity['banner_url']}}); border:solid; border-color:yellow;'>
+            @else
+            <div class='img' style='background-image:url({{$activity['banner_url']}}); border:solid; border-color:green;'>
+            @endif
 			    <div class='img2'>
-				    <p class='data_info'>[{{$activity['ingeschreven']}}]</p> 
+				    <p class='data_info'>{{$activity['ingeschreven']}}</p>
 				    <p class='data_info'>{{$activity['title']}}</p> 
 				    <p class='data_info'>{{$activity['description']}}</p>
 			    </div>
                 
                 <input type="hidden" name="activity_id" value="{{ $activity['id'] }}" >
                 <input type="hidden" name="member_id" value="{{ $userid }}" >
-
+                
 		    	<input type="submit" name="submit" id="submit" value="Select" class="btn btn-info pull-right">
 		    </div>
 	    </form>	
-    </div>
-	@endforeach
+    </div>      
+	@endforeach 
+                
+                
 
-
-	
 
 @endsection
