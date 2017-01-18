74e4d0cc7f10c73b56309fcb567db1ad9b17e01b
ROMY\Romy96 <romybijkerk1996@gmail.com>
2017-01-18 14:02:28 +0100

    Creëren event afgemaakt met afbeelding en de rest van de gegevens

--- create_event_action.php
+++ create_event_action.php
@@ -5,24 +5,37 @@ require 'inc/session.php';
 
 
 $eventadd = $_POST['event'];
+$afbeelding = $_POST['afbeelding'];
+$banner = $_POST['banner'];
+$startdatum = $_POST['startdatum'];
+$einddatum = $_POST['einddatum'];
 
-if (empty($_POST['event'])) {
-	$_SESSION['errors'][] = 'Er zijn velden niet ingevuld, probeer opnieuw.';
-	header('Location: create_event.php');
+if (empty($_POST['event']|| empty($afbeelding) || empty($banner) || empty($startdatum) || empty($locatie))) {
+    $_SESSION['errors'][] = 'Er zijn sommige velden die nog niet ingevuld zijn';
+    header('Location: create_event.php');
     exit;
 }
-if(isset($_POST['event'])){
-    $queryadd = $db->prepare("INSERT INTO events (title) VALUES ('".$_POST["event"]."')"); 
-    if ($queryadd->execute(array($eventadd))){
-        if ($queryadd->rowCount() > 1) {
-        $_SESSION['errors'][] = "U maakt teveel rijen";
-        }
-    }
+
+$sql = $db->prepare("SELECT * FROM events WHERE title=?");
+if ($sql->execute(array($eventadd))) {
+    if ( $sql->rowCount() > 0 ) {
+        $_SESSION['errors'][] = 'Sorry. Deze evenement bestaat al.';
+        header('Location: create_event.php');
+        exit;
+    } 
 }
-else{
-    $_SESSION['errors'][] = "Er is een fout opgetreden.";
-    header ('Location: create_event.php');
-    exit;
-}
+
+$sth = $db->prepare("INSERT INTO events (title, large_banner_url, small_banner_url, start_date, end_date) VALUES (?, ?, ?, ?, ?)");
+if ($sth->execute(array($eventadd, $banner, $afbeelding, $startdatum, $einddatum))) {
+    $_SESSION['errors'][] = 'De gegevens zijn ingevuld en opgeslagen in de database.';
     header('Location: Events_list.php');
+    exit;
+}
+else
+{
+    $_SESSION['errors'][] = 'Er is iets fout gegaan in de database. Probeer het later nog eens.';
+    header('Location: create_event.php');
+    exit;
+}
+?>
 
--- views/backend/events/create_event.blade.php
+++ views/backend/events/create_event.blade.php
@@ -6,16 +6,50 @@
 
     <div class="row">
         <form role="form" method="post" action="create_event_action.php">
-            <div class="col-lg-6">
-                <div class="well well-sm"><strong><span class="glyphicon glyphicon-asterisk"></span>Required Field</strong></div>
-                <div class="form-group">
-                    <label for="txt_evenement">Evenement naam:</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="event" name="event" placeholder="Vul een event in" required>
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
+                <div class="col-md-12">
+                    <div class="nav-tabs-custom">   <!-- white background -->
+                        <div class="box-body">      <!-- some whitespace -->
+                            <div class="box-body">  
+                                <div class="well well-sm"><strong><span class="glyphicon glyphicon-asterisk"></span>Required Field</strong></div>
+                                <div class="form-group">
+                                    <label for="txt_evenement">Evenement naam:</label>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" id="event" name="event" placeholder="Vul een event in" required>
+                                        <span class="input-group-addon"><span class="glyphicon glyphicon-asterisk"></span></span>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="afbeelding">Afbeelding achtergrond:</label>
+                                     <div class="input-group">
+                                        <input type="text" class="form-control" data-slug="source" placeholder="afbeelding achtergrond" id="afbeelding" name="afbeelding">
+                                        <span class="custom-file-control"></span>
+                                        <small class="text-muted">Put image URL from your documents or internet into the field.</small>
+                                    </div>
+                                </div>
+                                 <div class="form-group">
+                                    <label for="banner">Banner:</label>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" data-slug="source" placeholder="banner" id="banner" name="banner">
+                                        <span class="custom-file-control"></span>
+                                        <small class="text-muted">Put image URL from your documents or internet into the field.</small>
+                                </div>
+                                </div>
+                                <div class='form-group'>
+                                    <label for="startdatum">Startdatum:</label>
+                                     <div class="input-group">
+                                        <input class="form-control" data-slug="source" placeholder="startdatum" name="startdatum" type="date" id="startdatum">
+                                    </div>
+                                </div>
+                                <div class='form-group'>
+                                    <label for="startdatum">Einddatum:</label>
+                                     <div class="input-group">
+                                        <input class="form-control" data-slug="source" placeholder="einddatum" name="einddatum" type="date" id="einddatum">
+                                    </div>
+                                </div>
+                                <input type="submit" name="btn-submit" id="submit" value="Submit" class="btn btn-info pull-right">
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <input type="submit" name="btn-submit" id="submit" value="Submit" class="btn btn-info pull-right">
             </div>
         </form>
     </div>
